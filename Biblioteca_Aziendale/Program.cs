bf768ff2497b4b16f7de757767bffb7786190a82
andreavacca <andrea.vacca@outlook.com>
2022-09-14 17:49:07 +0200

    Modifica program.cs

--- Biblioteca_Aziendale/Program.cs
+++ Biblioteca_Aziendale/Program.cs
@@ -20,6 +20,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+//app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
